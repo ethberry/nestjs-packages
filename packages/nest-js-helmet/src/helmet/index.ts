aa75aae0807abdb7a1d837f1c973daf83aaeee1c
trejgun <trejgun@gmail.com>
2021-03-29 14:18:49 +0800

    add helmet middleware

---
+++ packages/nest-js-helmet/src/helmet/index.ts
@@ -0,0 +1,10 @@
+import helmet from "helmet";
+
+import {createModule} from "@trejgun/nest-js-create-middleware-module";
+
+// https://github.com/helmetjs/helmet/issues/279
+// https://github.com/helmetjs/helmet/issues/271
+
+export const HelmetModule = createModule<any>(options => {
+  return helmet(options);
+});
