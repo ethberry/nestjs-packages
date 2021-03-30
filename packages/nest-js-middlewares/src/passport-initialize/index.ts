6c5dfd331f2fb4db0ad6caec8593ecd297a5fe01
trejgun <trejgun@gmail.com>
2021-03-30 17:40:51 +0800

    drop middlewares

--- packages/nest-js-middlewares/src/passport-initialize/index.ts
+++
@@ -1,7 +0,0 @@
-import passport from "passport";
-
-import {createModule} from "@trejgun/nest-js-create-middleware-module";
-
-export const PassportInitialize = createModule(() => {
-  return passport.initialize();
-});
