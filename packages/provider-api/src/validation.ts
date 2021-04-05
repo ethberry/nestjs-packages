d28ecc3ae3be064b9a1d956fe57a8c7af394ce87
trejgun <trejgun@gmail.com>
2021-04-05 14:29:56 +0800

    add comment about nested validation

--- packages/provider-api/src/validation.ts
+++ packages/provider-api/src/validation.ts
@@ -5,14 +5,16 @@ export interface IValidationError {
   value: string;
   property: string;
   children: Array<any>;
-  constraints: Record<string, string>;
+  // constraint could be empty when error is in nested schema
+  constraints?: Record<string, string>;
 }
 
 export function localizeErrors(messages: Array<IValidationError>): Record<string, string> {
   return messages.reduce(
     (memo: Record<string, string>, message: IValidationError) => ({
       ...memo,
-      ...{[message.property]: `form.validations.${Object.values(message.constraints)[0]}`},
+      // TODO get errors from nested schemas
+      ...{[message.property]: `form.validations.${Object.values(message.constraints || {})[0]}`},
     }),
     {},
   );
