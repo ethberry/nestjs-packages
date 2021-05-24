2d4abb3a3cde1bf61be84df7cfd77e78c3369b1f
karafiziarthur <82943525+karafiziarthur@users.noreply.github.com>
2021-05-24 14:50:45 +0800

    fix: extract md text in markdown component from var to separated test.md file (#57)

--- .storybook/main.js
+++ .storybook/main.js
@@ -38,6 +38,7 @@ module.exports = {
             plugins: [
               "@babel/plugin-proposal-nullish-coalescing-operator",
               "@babel/plugin-proposal-optional-chaining",
+              "babel-plugin-inline-import"
             ],
           },
         },
