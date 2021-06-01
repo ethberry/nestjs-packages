c7c6f716581d599afd75fdbb40ce03c533e6b182
karafiziarthur <82943525+karafiziarthur@users.noreply.github.com>
2021-06-01 20:07:59 +0800

    fix: get errors from nested schema for localizeErrors (#65)

--- packages/provider-api/src/validation.ts
+++ packages/provider-api/src/validation.ts
@@ -12,17 +12,32 @@ export interface IValidationError {
 enum ValidationTypes {
   WHITELIST = "whitelistValidation",
 }
+function getParentProperty(parentProperty: string, property: string): string {
+  return parentProperty ? `${parentProperty}.${property}` : property;
+}
 
-export function localizeErrors(messages: Array<IValidationError>): Record<string, string> {
+export function localizeErrors(messages: Array<IValidationError>, parentProperty = ""): Record<string, string> {
   return messages.reduce((memo: Record<string, string>, message: IValidationError) => {
     const constraints = message.constraints || {};
     if (ValidationTypes.WHITELIST in constraints) {
       constraints[ValidationTypes.WHITELIST] = ValidationTypes.WHITELIST;
     }
-    return {
-      ...memo,
-      // TODO get errors from nested schemas
-      ...{[message.property]: `form.validations.${Object.values(constraints)[0]}`},
-    };
+
+    let result = {...memo};
+
+    if (Object.values(constraints)[0]) {
+      const validationKey = getParentProperty(parentProperty, message.property);
+      result[validationKey] = `form.validations.${Object.values(constraints)[0]}`;
+    }
+
+    if (message.children) {
+      const nextParentProperty = getParentProperty(parentProperty, message.property);
+      result = {
+        ...result,
+        ...localizeErrors(message.children, nextParentProperty),
+      };
+    }
+
+    return result;
   }, {});
 }
