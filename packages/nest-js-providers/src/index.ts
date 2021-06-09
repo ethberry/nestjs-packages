1d5599dfd2239256b6169db381f03de2931d1256
trejgun <trejgun@gmail.com>
2021-06-09 09:47:42 +0800

    feat: split provides and guards

--- packages/nest-js-providers/src/index.ts
+++ packages/nest-js-providers/src/index.ts
@@ -5,16 +5,6 @@ export * from "./decorators/roles";
 
 export * from "./filters/http";
 
-export * from "./guards/jwks.apple";
-export * from "./guards/jwks.google";
-export * from "./guards/jwt.facebook";
-export * from "./guards/jwt.google";
-export * from "./guards/jwt.http";
-export * from "./guards/local.facebook";
-export * from "./guards/local.google";
-export * from "./guards/local";
-export * from "./guards/login";
-
 export * from "./interceptors/empty-pipe";
 export * from "./interceptors/not-found";
 export * from "./interceptors/pagination";
