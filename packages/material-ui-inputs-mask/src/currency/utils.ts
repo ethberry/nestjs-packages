834d69d27c8a7639c7bc67d4727ead3409f43639
trejgun <trejgun@gmail.com>
2021-06-19 15:36:55 +0800

    fix: currency input

--- packages/material-ui-inputs-mask/src/currency/utils.ts
+++ packages/material-ui-inputs-mask/src/currency/utils.ts
@@ -1,4 +1,3 @@
 export const getFormattedCurrency = (value: string): number => Number.parseFloat(value) * 100;
 
-export const getNormalCurrency = (value: number): string =>
-  value === void 0 ? (value !== 0 ? value / 100 : 0).toString() : "";
+export const getNormalCurrency = (value: number): string => (value ? (value / 100).toString() : "0");
