658ee2151c5e0b1120e36be0187ea1ff36fe887c
trejgun <trejgun@gmail.com>
2021-07-02 18:07:25 +0800

    feat: pass themes to provider

--- packages/material-ui-provider-theme/src/provider.tsx
+++ packages/material-ui-provider-theme/src/provider.tsx
@@ -3,16 +3,19 @@ import * as mediaQuery from "css-mediaquery";
 
 import {createMuiTheme, CssBaseline, MuiThemeProvider} from "@material-ui/core";
 import {MuiMediaQueryList} from "@material-ui/core/useMediaQuery";
+import {PaletteOptions} from "@material-ui/core/styles/createPalette";
 
 import {ThemeContext, ThemeType} from "./context";
 import {dark, light} from "./palette";
 
 export interface IThemeProviderProps {
   type?: ThemeType;
+  darkPalette?: PaletteOptions;
+  lightPalette?: PaletteOptions;
 }
 
 export const ThemeProvider: FC<IThemeProviderProps> = props => {
-  const {type: defaultType = ThemeType.light, children} = props;
+  const {type: defaultType = ThemeType.light, darkPalette = dark, lightPalette = light, children} = props;
 
   const [type, setType] = useState<ThemeType>(defaultType);
 
@@ -41,7 +44,10 @@ export const ThemeProvider: FC<IThemeProviderProps> = props => {
         ssrMatchMedia,
       },
     },
-    palette: {light, dark}[type],
+    palette: {
+      light: lightPalette,
+      dark: darkPalette,
+    }[type],
     overrides: {
       // @ts-ignore
       MUIRichTextEditor: {
