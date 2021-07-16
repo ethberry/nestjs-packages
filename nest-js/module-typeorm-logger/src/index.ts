1629423ae1fb01c04ee79c3cd9a786ae616141a9
trejgun <trejgun@gmail.com>
2021-07-16 20:34:18 +0800

    fix: remove typeorm config

--- nest-js/module-typeorm-logger/src/index.ts
+++ nest-js/module-typeorm-logger/src/index.ts
@@ -1,4 +1,2 @@
-export * from "./typeorm-config.module";
-export * from "./typeorm-config.service";
 export * from "./typeorm-logger.module";
 export * from "./typeorm-logger.service";
--- nest-js/module-typeorm/src/typeorm-config.constants.ts
+++
@@ -1,3 +0,0 @@
-export enum ProviderType {
-  TYPEORM_OPTIONS = "TYPEORM_OPTIONS",
-}
