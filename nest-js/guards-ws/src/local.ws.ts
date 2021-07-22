6f53fbecc5c636651a1c13f28a432342c4f13e0d
trejgun <trejgun@gmail.com>
2021-07-22 08:13:01 +0800

    feat: local ws guard

---
+++ nest-js/guards-ws/src/local.ws.ts
@@ -0,0 +1,30 @@
+import {CanActivate, ExecutionContext, Injectable} from "@nestjs/common";
+import {Reflector} from "@nestjs/core";
+import {AuthGuard} from "@nestjs/passport";
+import {WsException} from "@nestjs/websockets";
+import {Socket} from "socket.io";
+
+@Injectable()
+export class WsLocalGuard extends AuthGuard("local") implements CanActivate {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+
+  public canActivate(context: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>("isPublic", [context.getHandler(), context.getClass()]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    const socket = context.switchToWs().getClient<Socket>();
+    // @ts-ignore
+    const isAuthenticated: boolean = socket.client.request.isAuthenticated();
+
+    if (!isAuthenticated) {
+      throw new WsException("unauthorized");
+    }
+
+    return isAuthenticated;
+  }
+}
