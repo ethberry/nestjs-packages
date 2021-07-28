8210a0c86b5e4b5023f68aee36733d4ca0fd8928
trejgun <trejgun@gmail.com>
2021-07-28 12:11:52 +0800

    fix: types

--- nest-js/module-winston-logdna/@types/logdna-winston/index.d.ts
+++ nest-js/module-winston-logdna/@types/logdna-winston/index.d.ts
@@ -1,1 +1,15 @@
-declare module "logdna-winston";
+declare module "logdna-winston" {
+  import TransportStream, { TransportStreamOptions } from "winston-transport";
+  import { ConstructorOptions } from "@logdna/logger";
+
+  type LogDNATransportOptions = Omit<ConstructorOptions, "level"> &
+    TransportStreamOptions & {
+      key?: string;
+    };
+
+  class LogDNATransport extends TransportStream {
+    constructor(options: LogDNATransportOptions);
+  }
+
+  export = LogDNATransport;
+}
