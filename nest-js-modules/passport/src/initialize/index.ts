c11a6e039c74cf2b7d13f8739dd6f9a1dda9d565
TrejGun <trejgun@gmail.com>
2021-09-04 22:51:16 +0800

    chore: remove passport module

--- nest-js-modules/passport/src/initialize/index.ts
+++
@@ -1,7 +0,0 @@
-import passport from "passport";
-
-import { createModule } from "@gemunion/nest-js-create-middleware-module";
-
-export const PassportInitialize = createModule(() => {
-  return passport.initialize();
-});
--- nest-js-modules/passport/src/session/index.ts
+++
@@ -1,8 +0,0 @@
-import { RequestHandler } from "express";
-import passport from "passport";
-
-import { createModule } from "@gemunion/nest-js-create-middleware-module";
-
-export const PassportSession = createModule(() => {
-  return passport.session() as RequestHandler;
-});
