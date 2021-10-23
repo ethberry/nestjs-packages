e9c9d7f7f8bb7679cc267b1dc2e81f421cc0a5c4
TrejGun <trejgun@gmail.com>
2021-10-23 16:48:50 +0800

    fix: throttler for gql

--- nest-js-modules/throttler/src/throttler.guard.gql.ts
+++ nest-js-modules/throttler/src/throttler.guard.gql.ts
@@ -1,6 +1,5 @@
-import { Injectable, ExecutionContext } from "@nestjs/common";
-import { ThrottlerGuard, ThrottlerException } from "@nestjs/throttler";
-import { GqlExecutionContext } from "@nestjs/graphql";
+import { ExecutionContext, Injectable } from "@nestjs/common";
+import { ThrottlerException, ThrottlerGuard } from "@nestjs/throttler";
 
 @Injectable()
 export class GqlThrottlerGuard extends ThrottlerGuard {
@@ -8,9 +7,10 @@ export class GqlThrottlerGuard extends ThrottlerGuard {
     req: Record<string, any>;
     res: Record<string, any>;
   } {
-    const gqlCtx = GqlExecutionContext.create(context);
-    const ctx = gqlCtx.getContext();
-    return { req: ctx.req, res: ctx.res }; // ctx.request and ctx.reply for fastify
+    return {
+      req: context.switchToHttp().getRequest(),
+      res: context.switchToHttp().getResponse(),
+    };
   }
 
   protected throwThrottlingException(): void {
