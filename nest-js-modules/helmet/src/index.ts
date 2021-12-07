8fa86d95d8cb6605906991d61b6377769d7a116d
TrejGun <trejgun@gmail.com>
2021-12-07 13:49:12 +0800

    feat: manually update helmet

--- nest-js-modules/helmet/src/index.ts
+++ nest-js-modules/helmet/src/index.ts
@@ -1,1 +1,5 @@
-export * from "./helmet";
+import helmet, { HelmetOptions } from "helmet";
+
+import { createModule } from "@gemunion/nest-js-create-middleware-module";
+
+export const HelmetModule = createModule<HelmetOptions>(helmet);
