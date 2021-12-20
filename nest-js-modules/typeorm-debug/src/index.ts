26d80a4934c855b6c5f439949c5dcd79469b6ff6
TrejGun <trejgun@gmail.com>
2021-12-20 11:14:18 +0800

    fix: reexport entities

--- nest-js-modules/typeorm-debug/src/index.ts
+++ nest-js-modules/typeorm-debug/src/index.ts
@@ -1,3 +1,3 @@
 export { GemunionTypeormDebugModule as GemunionTypeormModule } from "./typeorm.module";
 
-export { GemunionBaseEntity } from "@gemunion/nest-js-module-typeorm";
+export { IdBaseEntity, UuidBaseEntity, DateBaseEntity } from "@gemunion/nest-js-module-typeorm";
