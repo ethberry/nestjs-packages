68b407fb22b927015657f3270b92b31808c2f832
TrejGun <trejgun@gmail.com>
2022-01-14 20:38:08 +0800

    fix: add license to secret manager

--- nest-js-modules/secret-manager/src/secret-manager.module.ts
+++ nest-js-modules/secret-manager/src/secret-manager.module.ts
@@ -1,12 +1,23 @@
-import { createConfigurableDynamicRootModule } from "@gemunion/nest-js-create-dynamic-module";
 import { DynamicModule, Logger, Module } from "@nestjs/common";
+import { APP_GUARD } from "@nestjs/core";
+
+import { createConfigurableDynamicRootModule } from "@gemunion/nest-js-create-dynamic-module";
+import { LicenseGuard, LicenseModule } from "@gemunion/nest-js-module-license";
 
 import { SECRET_MANAGER_OPTIONS_PROVIDER } from "./secret-manager.constants";
 import { SecretManagerService } from "./secret-manager.service";
 import { ISecretManagerOptions } from "./interfaces";
 
 @Module({
-  providers: [Logger, SecretManagerService],
+  imports: [LicenseModule.deferred()],
+  providers: [
+    Logger,
+    SecretManagerService,
+    {
+      provide: APP_GUARD,
+      useClass: LicenseGuard,
+    },
+  ],
   exports: [SecretManagerService],
 })
 export class SecretManagerModule extends createConfigurableDynamicRootModule<
