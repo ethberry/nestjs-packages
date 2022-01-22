fd100accd6513e98a6ce5d951b6696cbfdcd8675
Yury Toporov <gemuniongroup@gmail.com>
2022-01-22 16:44:06 +0800

    refactor: renaming [skip ci]

--- scripts/renameo.sh
+++ scripts/renameo.sh
@@ -4,4 +4,6 @@ echo -e "\033[34mRenaming...\n\033[0m";
 
 set -e # this will cause the shell to exit immediately if any command exits with a nonzero exit value.
 
-find /home/runner/work/nestjs-packages/ -name '*obfuscated.js' | xargs -I{} rename -f 's/-obfuscated//' {}
+#find . -name '*obfuscated.js' | xargs -I{} rename -f 's/-obfuscated//' {}
+
+find . -name '*obfuscated.js' -exec bash -c 'mv "$1" "${1/-obfuscated/}"' _ {} \;
