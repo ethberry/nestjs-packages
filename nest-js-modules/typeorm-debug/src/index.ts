e758e5ca4bc3a2aadf801aecb5ce704a3b4b97cd
TrejGun <trejgun@gmail.com>
2022-02-14 12:21:07 +0800

    feat: typeorm helpers

--- nest-js-modules/typeorm-debug/src/index.ts
+++ nest-js-modules/typeorm-debug/src/index.ts
@@ -1,3 +1,1 @@
 export { GemunionTypeormDebugModule as GemunionTypeormModule } from "./typeorm.module";
-
-export { IdBaseEntity, UuidBaseEntity, DateBaseEntity } from "@gemunion/nest-js-module-typeorm";
