2be107bea9b61bc516b14e3c17cdf8370e874880
Yury Toporov <gemuniongroup@gmail.com>
2022-04-02 22:55:45 +0800

    refactor: test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,12 @@ echo -e "\033[34mTesting...\n\033[0m";
 
 set -e # this will cause the shell to exit immediately if any command exits with a nonzero exit value.
 
-export NODE_ENV=test
+export NODE_ENV=$NODE_ENV
+export GEMUNION_API_KEY=$GEMUNION_API_KEY
+export AWS_REGION=$AWS_REGION
+export AWS_S3_BUCKET=$AWS_S3_BUCKET
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 
 lerna bootstrap --hoist
 
