d38d99d49afaded89f1046b27d9f6c8a91718195
TrejGun <trejgun@gmail.com>
2022-04-29 20:40:46 +0800

    chore: fix semver

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,6 @@ echo -e "\033[34mTesting...\n\033[0m";
 
 set -e # this will cause the shell to exit immediately if any command exits with a nonzero exit value.
 
-echo $AWS_SECRET_ACCESS_KEY
 export NODE_ENV=test
 export GEMUNION_API_KEY=$GEMUNION_API_KEY
 export AWS_REGION=$AWS_REGION
