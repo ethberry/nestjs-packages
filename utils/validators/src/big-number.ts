f33f4a2b71ff921104c09491e7a6207fd861adc7
TrejGun <trejgun@gmail.com>
2022-05-03 10:26:08 +0800

    fix: IsBigNumber allowEmptyString

--- utils/validators/src/big-number.ts
+++ utils/validators/src/big-number.ts
@@ -1,15 +1,24 @@
-import { registerDecorator, ValidationOptions } from "class-validator";
+import { registerDecorator, ValidationArguments, ValidationOptions } from "class-validator";
 import { BigNumber } from "ethers";
 
-export function IsBigNumber(validationOptions?: ValidationOptions) {
+interface IBigNumberConstraints {
+  allowEmptyString: boolean;
+}
+
+export function IsBigNumber(constraints: Partial<IBigNumberConstraints> = {}, validationOptions?: ValidationOptions) {
   return (object: Record<string, any>, propertyName: string): void => {
     registerDecorator({
       name: "isBigNumber",
       target: object.constructor,
       propertyName,
+      constraints: [constraints],
       options: validationOptions,
       validator: {
-        validate(value: string): boolean {
+        validate(value: string, { constraints }: ValidationArguments): boolean {
+          const { allowEmptyString }: IBigNumberConstraints = constraints[0];
+          if (allowEmptyString && value === "") {
+            return true;
+          }
           try {
             BigNumber.from(value);
             return true;
