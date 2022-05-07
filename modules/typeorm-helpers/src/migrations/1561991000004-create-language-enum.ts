ed5c68c02cdcd8f73c47309bd8b894d5cd81d502
TrejGun <trejgun@gmail.com>
2022-05-07 17:36:22 +0800

    fix: add language to migration

--- modules/typeorm-helpers/src/migrations/1561991000004-create-language-enum.ts
+++ modules/typeorm-helpers/src/migrations/1561991000004-create-language-enum.ts
@@ -1,13 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
+import { EnabledLanguages } from "@gemunion/constants";
 
-export function createLanguageEnum(ns: string) {
+export function createLanguageEnum(ns: string, languages = Object.keys(EnabledLanguages)) {
   return class CreateLanguageEnum1561991000004 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<any> {
       await queryRunner.query(`
-        CREATE TYPE ${ns}.language_enum AS ENUM (
-          'RU',
-          'EN'
-        );
+        CREATE TYPE ${ns}.language_enum AS ENUM (${languages.join(",")});
       `);
     }
 
