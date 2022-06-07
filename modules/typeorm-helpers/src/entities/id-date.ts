38ae69acf0a5243ffa07f2cb76ff3d141d70514c
TrejGun <trejgun@gmail.com>
2022-06-07 17:57:52 +0800

    fix: entities

--- modules/typeorm-helpers/src/entities/id-date.ts
+++ modules/typeorm-helpers/src/entities/id-date.ts
@@ -1,12 +1,8 @@
-import { PrimaryGeneratedColumn } from "typeorm";
-import { decorate, Mixin } from "ts-mixer";
+import { Mixin } from "ts-mixer";
 
 import { IIdDateBase } from "@gemunion/types-collection";
 
 import { IdBaseEntity } from "./id";
 import { DateBaseEntity } from "./date";
 
-export abstract class IdDateBaseEntity extends Mixin(IdBaseEntity, DateBaseEntity) implements IIdDateBase {
-  @decorate(PrimaryGeneratedColumn())
-  public id: number;
-}
+export abstract class IdDateBaseEntity extends Mixin(IdBaseEntity, DateBaseEntity) implements IIdDateBase {}
--- modules/typeorm-helpers/src/entities/uuid-date.ts
+++ modules/typeorm-helpers/src/entities/uuid-date.ts
@@ -1,12 +1,8 @@
-import { PrimaryGeneratedColumn } from "typeorm";
-import { decorate, Mixin } from "ts-mixer";
+import { Mixin } from "ts-mixer";
 
 import { IUuidDateBase } from "@gemunion/types-collection";
 
 import { UuidBaseEntity } from "./uuid";
 import { DateBaseEntity } from "./date";
 
-export abstract class UuidDateBaseEntity extends Mixin(UuidBaseEntity, DateBaseEntity) implements IUuidDateBase {
-  @decorate(PrimaryGeneratedColumn())
-  public id: number;
-}
+export abstract class UuidDateBaseEntity extends Mixin(UuidBaseEntity, DateBaseEntity) implements IUuidDateBase {}
