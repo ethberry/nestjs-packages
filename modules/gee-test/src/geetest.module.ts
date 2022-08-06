6748d13da6e6c18d357da5546638e7bd364e3c99
trejgun <trejgun@gmail.com>
2022-08-06 18:18:40 +0800

    chore: secure gee test

--- modules/gee-test/src/geetest.module.ts
+++ modules/gee-test/src/geetest.module.ts
@@ -2,14 +2,16 @@ import { Module, Logger } from "@nestjs/common";
 import { HttpModule } from "@nestjs/axios";
 import { ConfigModule } from "@nestjs/config";
 
+import { LicenseModule, licenseProvider } from "@gemunion/nest-js-module-license";
+
 import { GeeTestController } from "./geetest.controller";
 import { GeeTestService } from "./geetest.service";
 import { ValidateGeeTest } from "./geetest.validator";
 
 @Module({
-  imports: [ConfigModule, HttpModule],
+  imports: [LicenseModule.deferred(), ConfigModule, HttpModule],
   controllers: [GeeTestController],
-  providers: [Logger, GeeTestService, ValidateGeeTest],
+  providers: [licenseProvider, Logger, GeeTestService, ValidateGeeTest],
   exports: [GeeTestService, ValidateGeeTest],
 })
 export class GeeTestModule {}
