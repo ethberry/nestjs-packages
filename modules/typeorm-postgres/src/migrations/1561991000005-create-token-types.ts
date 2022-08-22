f92a9e24fc3ae90c6e3f4f81d3e3fb9a08fa4f5a
TrejGun <trejgun@gmail.com>
2022-08-22 13:30:34 +0800

    chore: typeorm-helper -> typeorm-postgres

--- modules/typeorm-postgres/src/migrations/1561991000005-create-token-types.ts
+++ modules/typeorm-postgres/src/migrations/1561991000005-create-token-types.ts
@@ -8,9 +8,7 @@ export function createTokenTypes(ns: string) {
           'NATIVE',
           'ERC20',
           'ERC721',
-          'ERC721D',
           'ERC998',
-          'ERC998D',
           'ERC1155'
         );
       `);
