c53dfa129f61297441695353f319030cdc04b5f2
TrejGun <trejgun@gmail.com>
2023-04-10 14:31:27 +0800

    feat: cache ttl/max

--- modules/cache/src/cache.module.ts
+++ modules/cache/src/cache.module.ts
@@ -1,31 +1,28 @@
 import { Module } from "@nestjs/common";
-import { APP_INTERCEPTOR } from "@nestjs/core";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { RedisManager } from "@liaoliaots/nestjs-redis";
-
-import { CacheInterceptor, CacheModule, CacheModuleAsyncOptions } from "@nestjs/cache-manager";
+import { CacheModule, CacheModuleAsyncOptions } from "@nestjs/cache-manager";
 import { redisInsStore } from "cache-manager-ioredis-yet";
 
 import { LicenseModule, licenseProvider } from "@gemunion/nest-js-module-license";
+
 import { CACHE_STORE } from "./cache.constants";
 
 @Module({
   imports: [
     LicenseModule.deferred(),
     CacheModule.registerAsync<CacheModuleAsyncOptions>({
-      inject: [RedisManager],
-      useFactory: (redisManager: RedisManager) => {
+      imports: [ConfigModule],
+      inject: [ConfigService, RedisManager],
+      useFactory: (configService: ConfigService, redisManager: RedisManager) => {
         return {
+          ttl: configService.get<number>("CACHE_TTL", 3600),
+          max: configService.get<number>("CACHE_MAX", 1000),
           store: redisInsStore(redisManager.getClient(CACHE_STORE)),
         };
       },
     }),
   ],
-  providers: [
-    licenseProvider,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CacheInterceptor,
-    },
-  ],
+  providers: [licenseProvider],
 })
 export class GemunionCacheModule {}
