ea9c04079af003f31b15157fae0b0caccbe2bc79
TrejGun <trejgun@gmail.com>
2023-07-22 19:02:27 +0800

    fix: add address dto

--- utils/collection/src/account.ts
+++ utils/collection/src/account.ts
@@ -27,3 +27,28 @@ export class AccountOptionalDto {
   @decorate(Transform(({ value }: { value: string }) => (value === "" ? null : value.toLowerCase())))
   public account: string;
 }
+
+export class AddressDto {
+  @decorate(
+    ApiProperty({
+      type: String,
+    }),
+  )
+  @decorate(IsString({ message: "typeMismatch" }))
+  @decorate(IsEthereumAddress({ message: "patternMismatch" }))
+  @decorate(Transform(({ value }: { value: string }) => value.toLowerCase()))
+  public address: string;
+}
+
+export class AddressOptionalDto {
+  @decorate(
+    ApiPropertyOptional({
+      type: String,
+    }),
+  )
+  @decorate(IsOptional())
+  @decorate(IsString({ message: "typeMismatch" }))
+  @decorate(IsEthereumAddress({ message: "patternMismatch" }))
+  @decorate(Transform(({ value }: { value: string }) => (value === "" ? null : value.toLowerCase())))
+  public address: string;
+}
