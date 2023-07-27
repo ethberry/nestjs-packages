4e685b639f1d7d7b161255089c245bc4132710d8
TrejGun <trejgun@gmail.com>
2023-07-27 21:14:04 +0800

    fix: exports

--- modules/secret-manager-gcp/src/index.ts
+++ modules/secret-manager-gcp/src/index.ts
@@ -2,3 +2,4 @@ export * from "./interfaces";
 export * from "./secret-manager.module";
 export * from "./secret-manager.service";
 export * from "./secret-manager.constants";
+export * from "./signer";
