a706cc1ff5e7ed67a317996c16c97bec6624240a
TrejGun <trejgun@gmail.com>
2023-07-27 23:52:58 +0800

    fix: remove signer from secret manager

--- modules/secret-manager-gcp/src/index.ts
+++ modules/secret-manager-gcp/src/index.ts
@@ -2,4 +2,3 @@ export * from "./interfaces";
 export * from "./secret-manager.module";
 export * from "./secret-manager.service";
 export * from "./secret-manager.constants";
-export * from "./signer";
