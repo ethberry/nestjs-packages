c51166a7967189294739b3768be9c88516a224eb
TrejGun <trejgun@gmail.com>
2024-06-01 10:12:34 +0800

    docs: add comment about chainId

--- modules/typeorm-postgres/src/entities/deployable.ts
+++ modules/typeorm-postgres/src/entities/deployable.ts
@@ -9,6 +9,10 @@ export abstract class DeployableEntity extends IdDateBaseEntity implements IDepl
   @decorate(Column({ type: "varchar" }))
   public address: string;
 
+  // EVM
+  // The maximum value of Chain ID is 9,223,372,036,854,775,771 ( MAX_CHAIN_ID ).
+  // This value is floor(MAX_UINT64 / 2) - 36 , and is chosen to avoid overflow when performing uint64 math.
+  // For reference, a value of 0 or less is also disallowed.
   @decorate(Column({ type: "int" }))
   public chainId: number;
 }
