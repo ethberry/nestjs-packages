6d341c3338963b278512e5008d28d4aec1e6d5d3
TrejGun <trejgun@gmail.com>
2024-06-08 13:17:12 +0800

    fix: schedule validation, part 2

--- utils/validators/src/schedule.ts
+++ utils/validators/src/schedule.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { CronExpression } from "@nestjs/schedule";
-import { IsEnum } from "class-validator";
+import { IsEnum, IsOptional } from "class-validator";
 import { Transform } from "class-transformer";
 import { decorate } from "ts-mixer";
 
@@ -10,6 +10,18 @@ export class ScheduleDto {
       enum: CronExpression,
     }),
   )
+  @decorate(Transform(({ value }) => value as CronExpression))
+  @decorate(IsEnum(CronExpression, { message: "badInput" }))
+  public schedule: CronExpression;
+}
+
+export class ScheduleOptionalDto {
+  @decorate(
+    ApiProperty({
+      enum: CronExpression,
+    }),
+  )
+  @decorate(IsOptional())
   @decorate(Transform(({ value }) => value as CronExpression))
   @decorate(IsEnum(CronExpression, { message: "badInput" }))
   public schedule: CronExpression;
