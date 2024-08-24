c9a03502cbea5d97f82b017e8ee41047d60a3775
TrejGun <trejgun@gmail.com>
2024-08-24 10:31:43 +0800

    chore: manually updatethrottler

--- modules/throttler/src/throttler.guard.ws.ts
+++ modules/throttler/src/throttler.guard.ws.ts
@@ -1,28 +1,44 @@
 import { Injectable, ExecutionContext } from "@nestjs/common";
 import { ThrottlerGuard, ThrottlerException } from "@nestjs/throttler";
-import type { ThrottlerOptions } from "@nestjs/throttler";
+import type { ThrottlerRequest, ThrottlerLimitDetail } from "@nestjs/throttler";
 
 @Injectable()
 export class WsThrottlerGuard extends ThrottlerGuard {
-  async handleRequest(
-    context: ExecutionContext,
-    limit: number,
-    ttl: number,
-    throttler: ThrottlerOptions,
-  ): Promise<boolean> {
-    const client = context.switchToWs().getClient();
-    const ip = client._socket.remoteAddress;
-    const key = this.generateKey(context, ip, throttler.name || "default");
-    const { totalHits } = await this.storageService.increment(key, ttl);
+  async handleRequest(requestProps: ThrottlerRequest): Promise<boolean> {
+    const { context, limit, ttl, throttler, blockDuration, generateKey } = requestProps;
 
-    if (totalHits > limit) {
-      void this.throwThrottlingException();
+    const client = context.switchToWs().getClient();
+    const tracker = client._socket.remoteAddress;
+    const key = generateKey(context, tracker, throttler.name || "default");
+    const { totalHits, timeToExpire, isBlocked, timeToBlockExpire } = await this.storageService.increment(
+      key,
+      ttl,
+      limit,
+      blockDuration,
+      throttler.name || "default",
+    );
+
+    // Throw an error when the user reached their limit.
+    if (isBlocked) {
+      await this.throwThrottlingException(context, {
+        limit,
+        ttl,
+        key,
+        tracker,
+        totalHits,
+        timeToExpire,
+        isBlocked,
+        timeToBlockExpire,
+      });
     }
 
     return true;
   }
 
-  protected throwThrottlingException(): Promise<void> {
+  protected throwThrottlingException(
+    _context: ExecutionContext,
+    _throttlerLimitDetail: ThrottlerLimitDetail,
+  ): Promise<void> {
     throw new ThrottlerException("tooManyRequests");
   }
 }
