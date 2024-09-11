074e3b8929df5365b67e4a81896377936123dfbd
TrejGun <trejgun@gmail.com>
2024-09-11 19:32:08 +0800

    fix: move eth-utils to common

--- utils/utils-eth/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./block-await";
-export * from "./decode-result";
-export * from "./map-series";
-export * from "./abi-encode";
--- utils/utils-eth/src/map-series.ts
+++
@@ -1,8 +0,0 @@
-export const mapSeries = <T>(tasks: Array<() => Promise<T>>): Promise<Array<T>> => {
-  return tasks.reduce(
-    (promiseChain, currentTask) => {
-      return promiseChain.then(chainResults => currentTask().then(currentResult => [...chainResults, currentResult]));
-    },
-    Promise.resolve([] as Array<T>),
-  );
-};
